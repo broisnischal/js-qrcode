48863343b14a73064fe9efcf86b978929eb44ed7
neeswebservices <techneesofficial@gmail.com>
2022-05-29 23:00:24 +0545

    main

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ function func() {
 	if (x == '') {
 		alert('Please enter a value');
 	} else {
-		function func() {
-			var x = document.getElementById('input').value;
-			if (x == '') {
-				alert('Please enter a value');
-			} else {
-				var url = `https://api.qrserver.com/v1/create-qr-code/?size=55x55&data=${x}`;
-				var img = document.getElementById('img');
-				var generatedText = document.getElementById('successText');
-				generatedText.classList.remove('hide');
-				img.src = url;
-				generatedText.innerHTML = 'Generated successfully!';
-			}
-		}
+		var url = `https://api.qrserver.com/v1/create-qr-code/?size=55x55&data=${x}`;
+		var img = document.getElementById('img');
+		var generatedText = document.getElementById('successText');
+		generatedText.classList.remove('hide');
+		img.src = url;
+		generatedText.innerHTML = 'Generated successfully!';
+		console.log('Code generated with content ' + x);
 	}
 }
+// var link = document.createElement('a');
+// link.download = 'qr.png';
+// link.href = url;
+// link.click();
+// link.remove();
