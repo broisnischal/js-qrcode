4dda004a78ff077025df2d47acbf9a1a2c04b5bd
neeswebservices <techneesofficial@gmail.com>
2022-05-29 22:44:37 +0545

    2021

---
+++ app.js
@@ -0,0 +1,14 @@
+function func() {
+	var x = document.getElementById('input').value;
+	if (x == '') {
+		alert('Please enter a value');
+	} else {
+		var url = `https://api.qrserver.com/v1/create-qr-code/?size=55x55&data=${x}`;
+		var img = document.getElementById('img');
+		var generatedText = document.getElementById('successText');
+		generatedText.classList.remove('hide');
+		img.src = url;
+		generatedText.innerHTML = 'Generated successfully!';
+		console.log('Code generated with content ' + x);
+	}
+}
